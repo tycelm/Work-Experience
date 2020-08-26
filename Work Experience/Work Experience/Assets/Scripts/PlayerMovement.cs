0b7a9de458b8c22cffaac4371f79b8d7622d2317
tycelm <70135829+tycelm@users.noreply.github.com>
2020-08-26 18:43:31 -0400

    Animator works now

--- Work Experience/Work Experience/Assets/Scripts/PlayerMovement.cs
+++ Work Experience/Work Experience/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,15 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
     float horizontalMove = 0f;
     public float runSpeed = 40f;
+    public Animator animator;
     bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Crouch"))
         {
